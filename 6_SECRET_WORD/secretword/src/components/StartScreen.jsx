e41239d2f77e9d6a15267e2318e8ef672c8eceb6
LorranDC <lorrandias150@gmail.com>
2025-01-16 00:12:19 -0300

    Projeto Finalizado!

--- 6_SECRET_WORD/secretword/src/components/StartScreen.jsx
+++ 6_SECRET_WORD/secretword/src/components/StartScreen.jsx
@@ -1,12 +1,11 @@
 import "./StartScreen.css";
 
-const StartScreen = () => {
+const StartScreen = ({startGame}) => {
   return (
     <div className="start" >
         <h2>Secret Word</h2>
         <p>Clique no botão abaixo para começar a jogar</p>
-        <button>Começar o jogo</button>
-
+        <button onClick={startGame} >Começar o jogo</button>
     </div>
   )
 }
