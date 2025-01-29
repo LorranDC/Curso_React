13a586663434000e8ee77c8ad9311d852888c9e5
LorranDC <lorrandias150@gmail.com>
2025-01-28 22:07:55 -0300

    seção concluída

--- 9_CONTEXT_API/context/src/pages/About.jsx
+++ 9_CONTEXT_API/context/src/pages/About.jsx
@@ -1,8 +1,27 @@
+//import { useContext } from "react"
+import ChangeCounter from "../components/ChangeCounter"
 
+//import { CounterContext } from "../context/CounterContext"
+
+//4 - refatorando com hook
+import { useCounterContext } from "../hooks/useCounterContext"
+
+import { userTitleColorContext } from "../hooks/useTitleColorContext";
 
 const About = () => {
+  //const {counter} = useContext(CounterContext);
+
+  const {counter} = useCounterContext();
+
+  //5 - context mais complexo
+  const {color, dispatch} = userTitleColorContext();
+
   return (
-    <div>About</div>
+    <div>
+      <h1 style={{ color: color }} >About</h1>
+        <p>Valor do contador: {counter}</p>
+
+    </div>
   )
 }
 
--- 9_CONTEXT_API/context/src/pages/Home.jsx
+++ 9_CONTEXT_API/context/src/pages/Home.jsx
@@ -1,12 +1,37 @@
-import { useContext } from "react"
-import {CounterContext} from "../context/CounterContext"
+// import { useContext } from "react"
+import ChangeCounter from "../components/ChangeCounter"
+// import {CounterContext} from "../context/CounterContext"
+
+import {useCounterContext} from "../hooks/useCounterContext"
+
+
+//5 - context mais complexo
+import { userTitleColorContext } from "../hooks/useTitleColorContext";
 
 const Home = () => {
-    const {counter} = useContext(CounterContext);
+    // const {counter} = useContext(CounterContext);
+    const {counter} = useCounterContext(); 
+
+    //5 - context mais complexo
+    const {color, dispatch} = userTitleColorContext();
+
+    //6 - alterando state complexo
+    const setTitleColor = (color) => {
+      dispatch({type: color});
+    }
+
+
     return (
     <div>
-        <h1>Home</h1>
+        <h1 style={{ color: color }} >Home</h1>
         <p>Valor do contador: {counter}</p>
+        {/* 3 - alterando valor contexto */}
+        <ChangeCounter/>
+        {/* 6 - alterando contexto complexo */}
+        <div>
+          <button onClick={() => setTitleColor("RED")} >Vermelho</button>
+          <button onClick={() => setTitleColor("BLUE")} >Azul</button>
+        </div>
     </div>
   )
 }
