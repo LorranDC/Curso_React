b8afafacc02e1fbebd9a017033a5f4d52959df92
LorranDC <lorrandias150@gmail.com>
2025-03-17 23:44:07 -0300

    Inicia novo projeto de MiniBlog

---
+++ 10_MINIBLOG/miniblog/src/firebase/config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDbHVGcR9sSe--b1lqR4WVu90oQ4KaE7KE",
+  authDomain: "miniblog-ceb2f.firebaseapp.com",
+  projectId: "miniblog-ceb2f",
+  storageBucket: "miniblog-ceb2f.firebasestorage.app",
+  messagingSenderId: "444254294081",
+  appId: "1:444254294081:web:e7df4a6b39b0ca5652f6c0"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export {db};
