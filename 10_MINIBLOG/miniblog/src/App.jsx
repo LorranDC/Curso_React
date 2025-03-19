db5574beb6c8c522911ef0ea42b664dff5701cdb
LorranDC <lorrandias150@gmail.com>
2025-03-18 23:35:13 -0300

    footer, header e CSS dos componentes

--- 10_MINIBLOG/miniblog/src/App.jsx
+++ 10_MINIBLOG/miniblog/src/App.jsx
@@ -3,9 +3,15 @@ import './App.css'
 
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
+//components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 //pages
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+
+
 
 function App() {
 
@@ -13,6 +19,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
+      <Navbar/>
       <div className='container' >
         <Routes>
 
@@ -21,6 +28,7 @@ function App() {
 
         </Routes>
       </div>
+      <Footer />
       </BrowserRouter>
     </>
   );
