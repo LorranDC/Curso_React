839b11b7c8b73b81cddf8182d9382031f6aecb68
LorranDC <lorrandias150@gmail.com>
2025-04-13 20:17:53 -0300

    criar posts

--- 10_MINIBLOG/miniblog/src/pages/CreatePost/CreatePost.jsx
+++ 10_MINIBLOG/miniblog/src/pages/CreatePost/CreatePost.jsx
@@ -3,6 +3,7 @@ import styles from './CreatePost.module.css'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAuthValue } from "../../context/AuthContext"
+import { useInsertDocument } from '../../hooks/useInsertDocument'
 
 
 
@@ -13,8 +14,47 @@ const CreatePost = () => {
   const [tags, setTags] = useState([])
   const [formError, setFormError] = useState("")
 
+  const { insertDocument, response } = useInsertDocument("posts");
+
+  const navigate = useNavigate();
+
+  const {user } = useAuthValue();
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    setFormError("")
+
+    //validate image URL
+    try {
+      new URL(image)
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL.");
+    }
+
+
+
+    //criar o array de tags
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
+
+    //checar todos os valores
+    if(!title || !image || !tags || !body) {
+      setFormError("Por favor preencha todos os campos!");
+    }
+
+    if(formError) return;
+
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tagsArray,
+      uid: user.uid,
+      createdBy: user.displayName
+    })
+
+    //redirect to Home Page
+    navigate("/");
   }
 
 
@@ -67,15 +107,18 @@ const CreatePost = () => {
             value={tags}
           />
         </label>
-        <button className="btn" >Cadastrar</button>
-        {/* {!loading && <button className="btn" >Cadastrar</button>}
-                {loading && <button className="btn" disabled>Aguarde...</button>}
-                {error && <p className="error" >{error}</p> } */}
-
-
+        
+        {!response.loading && <button className="btn" >Cadastrar</button>}
+        {response.loading && (
+          <button className="btn" disabled>
+          Aguarde...
+          </button>
+          )}
+          {response.error && <p className="error" >{response.error}</p> }
+          {formError && <p className="error" >{formError}</p> }
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default CreatePost
+export default CreatePost;
