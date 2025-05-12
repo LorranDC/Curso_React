fde52b972215cc6bf08ce246a5639217c4afc27c
LorranDC <lorrandias150@gmail.com>
2025-05-11 23:51:48 -0300

    carregar Posts

--- 10_MINIBLOG/miniblog/src/App.jsx
+++ 10_MINIBLOG/miniblog/src/App.jsx
@@ -23,6 +23,7 @@ import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import CreatePost from './pages/CreatePost/CreatePost';
 import Dashboard from './pages/Dashboard/Dashboard';
+import Search from './pages/Search/Search';
 
 
 
@@ -54,6 +55,7 @@ function App() {
 
               <Route path='/' element={<Home />} />
               <Route path='/about' element={<About />} />
+              <Route path='search' element={<Search />} />
               <Route path='/login' element={!user ? <Login /> : <Navigate to="/" />} />
               <Route path='/register' element={!user ? <Register /> : <Navigate to="/" />} />
               <Route path='/posts/create' element={user ? <CreatePost /> : <Navigate to="/login" />} />
