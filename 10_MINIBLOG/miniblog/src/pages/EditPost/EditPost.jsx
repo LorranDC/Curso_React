6f0f26f5ec9a6e6b12a4de51fe70b7cfae1c09b5
LorranDC <lorrandias150@gmail.com>
2025-05-19 23:34:26 -0300

    Atualizando o Post

--- 10_MINIBLOG/miniblog/src/pages/EditPost/EditPost.jsx
+++ 10_MINIBLOG/miniblog/src/pages/EditPost/EditPost.jsx
@@ -3,9 +3,8 @@ import styles from './EditPost.module.css'
 import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { useAuthValue } from "../../context/AuthContext"
-import { useInsertDocument } from '../../hooks/useInsertDocument'
 import { useFetchDocument } from '../../hooks/useFetchDocument'
-
+import { useUpdateDocument } from '../../hooks/useUpdateDocument'
 
 
 
@@ -31,7 +30,7 @@ const EditPost = () => {
     }
 
   }, [post])
-  const { insertDocument, response } = useInsertDocument("posts");
+  const { updateDocument, response } = useUpdateDocument("posts");
 
   const navigate = useNavigate();
 
@@ -60,23 +59,24 @@ const EditPost = () => {
 
     if(formError) return;
 
-
-    insertDocument({
+    const data = {
       title,
       image,
       body,
       tagsArray,
       uid: user.uid,
       createdBy: user.displayName
-    })
+    }
+
+    updateDocument(id, data);
 
     //redirect to Home Page
-    navigate("/");
+    navigate("/dashboard");
   }
 
 
   return (
-    <div className={styles.create_post} >
+    <div className={styles.edit_post} >
       {post && (
         <>
           <h2>Editando Post: {post.title} </h2>
