7e7b480ede7a96a16b68812e54b5fbac62ba36ae
LorranDC <lorrandias150@gmail.com>
2025-09-03 15:57:30 -0300

    validações de criação de usuário

--- 12_REACTGRAM/backend/middlewares/handleValidation.js
+++ 12_REACTGRAM/backend/middlewares/handleValidation.js
@@ -1,8 +1,7 @@
 const {validationResult} = require("express-validator");
 
 const validate = (req, res, next) => {
-
-    const errors = validationResult(req)
+    const errors = validationResult(req);
 
     if(errors.isEmpty()) {
         return next();
@@ -10,7 +9,7 @@ const validate = (req, res, next) => {
 
     const extractedErrors = [];
 
-    errors.array.map((err) => extractedErrors.push(err.msg))
+    errors.array().map((err) => extractedErrors.push(err.msg))
 
     return res.status(422).json({
         errors: extractedErrors,
