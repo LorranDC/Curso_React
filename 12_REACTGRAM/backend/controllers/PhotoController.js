4902c8835f2cc969d61989d3580aac52cd22cd30
LorranDC <lorrandias150@gmail.com>
2025-09-17 00:07:06 -0300

    Like em foto

--- 12_REACTGRAM/backend/controllers/PhotoController.js
+++ 12_REACTGRAM/backend/controllers/PhotoController.js
@@ -134,6 +134,34 @@ const updatePhoto = async(req, res) => {
     res.status(200).json({photo, message: ["Foto atualizada com sucesso!"]})
 }
 
+
+//Like functionality
+const likePhoto = async(req, res) => {
+    
+    const {id} = req.params
+
+    const reqUser = req.user
+
+    const photo = await Photo.findById(id);
+
+    //Check if photo exists
+    if(!photo) {
+        res.status(404).json({ errors: ["Foto não encontrada"]})
+        return
+    }
+
+    if(photo.likes.includes(reqUser._id)) {
+        res.status(422).json({ errors: ["Você já curtiu a foto."] });
+    }
+
+    //Put user id in likes array
+    photo.likes.push(reqUser._id)
+
+    photo.save()
+
+    res.status(200).json({photoId: id, userId: reqUser._id, message: "A foto foi curtida."});
+}
+
 module.exports = {
     insertPhoto,
     deletePhoto,
@@ -141,4 +169,5 @@ module.exports = {
     getUserPhotos,
     getPhotoById,
     updatePhoto,
+    likePhoto,
 }
