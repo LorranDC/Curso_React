8b66966324fedd91310b68c37cc41fce6bf57788
LorranDC <lorrandias150@gmail.com>
2025-09-18 23:51:09 -0300

    Criando componentes de navbar

--- 12_REACTGRAM/frontend/src/App.js
+++ 12_REACTGRAM/frontend/src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 
 import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom'
 
+//Components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+//Pages
 import Home from './pages/Home/Home';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
@@ -11,11 +16,13 @@ function App() {
     <div className="App">
       
       <BrowserRouter>
+      <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>}  />
           <Route path='/login' element={<Login/>}  />
           <Route path='/register' element={<Register/>}  />
         </Routes>
+        <Footer/>
       </BrowserRouter>
 
 
