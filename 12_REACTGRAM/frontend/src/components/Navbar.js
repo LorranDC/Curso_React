b379f5986b921dfe2323cef0324b7a669f7811e3
LorranDC <lorrandias150@gmail.com>
2025-09-20 23:47:38 -0300

    CSS Global

--- 12_REACTGRAM/frontend/src/components/Navbar.js
+++ 12_REACTGRAM/frontend/src/components/Navbar.js
@@ -8,8 +8,7 @@ const Navbar = () => {
     return (
         <nav id="nav">
             <Link to="/" >ReactGram</Link>
-            <form action="
-            ">
+            <form id="search-form">
                 <BsSearch/>
                 <input type="text" placeholder="Pesquisar"/>
             </form>
