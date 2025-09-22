f911b6f899c479ac3d1fecb4c61ca98cbb5fc0e9
LorranDC <lorrandias150@gmail.com>
2025-09-21 23:00:36 -0300

    Exibindo validação do usuário

--- 12_REACTGRAM/frontend/src/pages/Auth/Register.js
+++ 12_REACTGRAM/frontend/src/pages/Auth/Register.js
@@ -2,7 +2,7 @@ import "./Auth.css";
 
 //Components
 import { Link } from 'react-router-dom'
-
+import  Message  from '../../components/Message'
 //Hooks
 import { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from "react-redux";
@@ -60,9 +60,11 @@ const Register = () => {
       <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} value={email || "" } />
       <input type="password" placeholder="Senha" onChange={(e) => setPassword(e.target.value)} value={password || "" } />
       <input type="password" placeholder="Confirme a senha" onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword || "" } />
-      <input type="submit" placeholder="Cadastrar" />
+     
 
-
+      {! loading &&  <input type="submit" value="Cadastrar" /> }
+      {loading &&  <input type="submit" value="Aguarde..." disabled />}
+      {error && <Message msg={error} type="error" />}
     </form>
     <p>Já possui uma conta?
       <Link to="/login"> Clique aqui.
