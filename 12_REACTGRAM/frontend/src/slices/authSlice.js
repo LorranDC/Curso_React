ed97f5e02ad1999227e99429d3083b49bbe3b333
LorranDC <lorrandias150@gmail.com>
2025-11-12 23:16:41 -0300

    Função de Login

--- 12_REACTGRAM/frontend/src/slices/authSlice.js
+++ 12_REACTGRAM/frontend/src/slices/authSlice.js
@@ -24,10 +24,23 @@ export const register = createAsyncThunk("auth/register", async (user, thunkAPI)
 }
 );
 
-//Logou an user
+//Logout an user
 export const logout = createAsyncThunk("auth/logout", async () => {
     await authService.logout();
 })
+
+//Sign in an user and login
+export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
+
+    const data = await authService.login(user)
+
+    //check for errors
+    if (data.errors) {
+        return thunkAPI.rejectWithValue(data.errors[0])
+    }
+    return data
+}
+);
 
 export const authSlice = createSlice({
     name: "auth",
@@ -41,7 +54,8 @@ export const authSlice = createSlice({
     },
 
     extraReducers: (builder) => {
-        builder.addCase(register.pending, (state) => {
+        builder
+        .addCase(register.pending, (state) => {
             state.loading = true
             state.error = false
         }).addCase(register.fulfilled, (state, action) => {
@@ -61,6 +75,20 @@ export const authSlice = createSlice({
             state.error = null
             state.user = null
         })
+        .addCase(login.pending, (state) => {
+            state.loading = true
+            state.error = false
+        }).addCase(login.fulfilled, (state, action) => {
+            state.loading = false 
+            state.success = true
+            state.error = null
+            state.user = action.payload
+        })
+        .addCase(login.rejected, (state, action) => {
+            state.loading = false
+            state.error = action.payload
+            state.user = null
+        });
     },
 });
 
