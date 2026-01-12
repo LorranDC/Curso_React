6c4b85e96e4a66c212b11e0fccf7bd7bdea7adba
LorranDC <lorrandias150@gmail.com>
2026-01-12 01:38:22 -0300

    Criando e estilizando modal. Add evento para fechar modal

--- REACT_COM_TS/12_todo_react/src/App.tsx
+++ REACT_COM_TS/12_todo_react/src/App.tsx
@@ -11,6 +11,7 @@ import styles from "./App.module.css";
 
 //Interface
 import { ITask } from "./interfaces/Task";
+import Modal from "./components/Modal";
 
 function App() {
   const [taskList, setTaskList] = useState<ITask[]>([]);
@@ -23,8 +24,24 @@ function App() {
     )
   }
 
+  const hideOrShowModal = (display:boolean) => {
+    const modal = document.querySelector("#modal")
+
+    if(display) {
+      modal!.classList.remove("hide")
+    }
+    else {
+      modal!.classList.add("hide")
+    }
+  }
+
+  const editTask = ():void => {
+    hideOrShowModal(true)
+  }
+
   return (
     <div>
+      <Modal children={<TaskForm btnText="Editar Tarefa" taskList={taskList} />}/>
       <Header />
 
       <main className={styles.main}>
@@ -39,7 +56,7 @@ function App() {
 
         <div>
           <h2>Suas tarefas:</h2>
-          <TaskList taskList={taskList} handleDelete={deleteTask}/>
+          <TaskList taskList={taskList} handleDelete={deleteTask} handleEdit={editTask}/>
         </div>
       </main>
       <Footer />
